347e605ad7b185f8bbd2cf1277edf0f4159d9121
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-22 01:42:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students 
-      WHERE grade BETWEEN 9 AND 11
+      WHERE grade < 12 
       SQL
       
       DB[:conn].execute(sql, grade).map do |row|
